c7a9bd788dae79f0f7990ab551b76f199b6e1c24
Dylan Landry <dylanglandry@gmail.com>
2019-07-26 17:10:56 -0400

    Make test values purposely arbitrary.

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -13,26 +13,26 @@ describe('api', () => {
       it('contains passed url', () => {
         const expected = 'http://url'
         const graphQLGetRequest = GraphQLRequest(
-          '{ posts }', 
+          'abc', 
           { graphQLEndpoint: expected }
         )
-        
         graphQLGetRequest.url.should.equal(expected)
       })
       
       it('contains passed query in POST body', async () => {
-        const request = GraphQLRequest('{ posts }')
+        const expected = 'abc'
+        const request = GraphQLRequest(expected)
         const bodyJson = await request.json()
-        bodyJson.query.should.equal('{ posts }')
+        bodyJson.query.should.equal(expected)
       })
 
       it('contains application/json Content-Type header', () => {
-        const request = GraphQLRequest('{ posts }')
+        const request = GraphQLRequest('abc')
         request.headers.get('Content-Type').should.equal('application/json')
       })
 
       it('contains POST http method', () => {
-        const request = GraphQLRequest('{ posts }')
+        const request = GraphQLRequest('abc')
         request.method.should.equal('POST')
       })
 
